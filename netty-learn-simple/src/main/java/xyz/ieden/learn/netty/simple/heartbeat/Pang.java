90d36ccf520ecff4160b0a255b7fb6aa0e1f73a8
gavinlen <gavin_len@163.com>
2020-11-30 21:51:23 +0800

    心跳检测 Demo

---
+++ netty-learn-simple/src/main/java/xyz/ieden/learn/netty/simple/heartbeat/Pang.java
@@ -0,0 +1,11 @@
+package xyz.ieden.learn.netty.simple.heartbeat;
+
+/**
+ * Pang
+ *
+ * @author gavin
+ * @version 1.0
+ * @datetime 2020/11/30 21:47
+ */
+public class Pang {
+}
---
+++ netty-learn-simple/src/main/java/xyz/ieden/learn/netty/simple/heartbeat/Ping.java
@@ -0,0 +1,11 @@
+package xyz.ieden.learn.netty.simple.heartbeat;
+
+/**
+ * Service
+ *
+ * @author gavin
+ * @version 1.0
+ * @datetime 2020/11/30 21:47
+ */
+public class Ping {
+}
